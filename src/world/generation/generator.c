b9e2c1a3c53fd8c5102cdee6b3ff7d5841470f50
coluh <coluh.b@gmail.com>
2025-05-12 13:52:21 +0800

    FIX: convert sRGB to linear, add gamma correction

--- src/world/generation/generator.c
+++ src/world/generation/generator.c
@@ -5,7 +5,8 @@
 void generator_default(Chunk *chunk) {
 	for (int dx = 0; dx < CHUNK_SIZE; dx++) {
 		for (int dz = 0; dz < CHUNK_SIZE; dz++) {
-			int top= rand_int(18, 21);
+			// int top= rand_int(18, 21);
+			int top= 20;
 			for (int dy = 0; dy < CHUNK_SIZE; dy++) {
 				int y = chunk->y + dy;
 				BlockID id = y > top ? BLOCK_Air : (y > top-1 ? BLOCK_GrassBlock : (y > top-4 ? BLOCK_Dirt : BLOCK_Stone));
